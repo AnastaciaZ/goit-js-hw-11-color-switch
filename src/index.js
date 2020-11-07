0dd43d3ff9472ad8fdcc62e355d2af9363a8d748
AnastaciaZ <zaikina.anastacia@gmail.com>
2020-11-07 14:07:58 +0200

    switch

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const start = document.querySelector('[data-action="start"]');
+const stop = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+start.addEventListener('click', onStart);
+
+function onStart() { 
+    start.disabled = true;
+    timerId = setInterval(() => {
+    body.style.background=colors[randomIntegerFromInterval(0, colors.length-1)]
+    }, 1000);
+}
+
+stop.addEventListener('click', onStop);
+
+function onStop() { 
+    clearInterval(timerId);
+    start.disabled = false;
+}
